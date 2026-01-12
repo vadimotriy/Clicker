013377f1cdfe8fc1ef5752e3441fec08d281a97c
Vadim <vblinov2009.rus@yandex.ru>
2026-01-12 12:57:32 +0300

    [BACKEND][FRONTEND] добавлена кнопка и счетчик, добавлена базовая обработка кнопки

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.clicker;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private int num = 0;
+
 
     private static final String LOG_TAG = "ClickerMainActivity";
 
@@ -14,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(LOG_TAG, "Базовая настройка завершена");
+
+        Button button = findViewById(R.id.button);
+        TextView textview = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOG_TAG, "Клик");
+                ++num;
+
+                textview.setText(Integer.toString(num));
+            }
+        });
     }
 }
